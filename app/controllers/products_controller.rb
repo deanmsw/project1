53f0da4e2b71551bb38e0ca7ca595b576c35e97a
Dean Scotthorne <dean@mysuperiorwebsite.com>
2021-10-27 09:09:55 +1100

    second commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
 
 def index
   if params[:search]
-    @products = Product.find(:all, :conditions => ['name LIKE ?', "%#{params[:search]}%"])
+    @products = Product.where('name LIKE ?', "%#{params[:search]}%")
   else
     @products = Product.all
   end
